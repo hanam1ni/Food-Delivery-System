77d65e14879ff7b0da0098709277d5dfd7483007
nonamexx <mrkom9@gmail.com>
2016-11-01 17:08:50 +0700

    Draft
    try to show rating pic
    add menu and comment tab

--- FDS-laravel/resources/views/restaurant.blade.php
+++ FDS-laravel/resources/views/restaurant.blade.php
@@ -1,89 +1,106 @@
 <html lang="en">
-	<head>
-		<meta charset="utf-8">
-		<title>Software Engineer</title>
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<meta name="description" content="">
-		<!--[if ie]><meta content='IE=8' http-equiv='X-UA-Compatible'/><![endif]-->
-		<!-- bootstrap -->
-		<link href="css/bootstrap.min.css" rel="stylesheet">      
-		<link href="css/bootstrap-responsive.min.css" rel="stylesheet">
-		
-		<link href="css/bootstrappage.css" rel="stylesheet"/>
-		
-		<!-- global styles -->
-		<link href="css/flexslider.css" rel="stylesheet"/>
-		<link href="css/browse.css" rel="stylesheet"/>
+  <head>
+    <meta charset="utf-8">
+    <title>Software Engineer</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <!--[if ie]><meta content='IE=8' http-equiv='X-UA-Compatible'/><![endif]-->
+    <!-- bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">      
+    <link href="css/bootstrap-responsive.min.css" rel="stylesheet">
+    
+    <link href="css/bootstrappage.css" rel="stylesheet"/>
+    
+    <!-- global styles -->
+    <link href="css/flexslider.css" rel="stylesheet"/>
+    <link href="css/browse.css" rel="stylesheet"/>
 
-		<link href="css/restaurant.css" rel="stylesheet"/>
+    <link href="css/restaurant.css" rel="stylesheet"/>
+    <!-- scripts -->
+    <script src="js/jquery-1.7.2.min.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>       
+    <script src="js/superfish.js"></script> 
 
-		<!-- scripts -->
-		<script src="js/jquery-1.7.2.min.js"></script>
-		<script src="bootstrap/js/bootstrap.min.js"></script>				
-		<script src="js/superfish.js"></script>	
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.19.0/TweenMax.min.js"></script>
 
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.19.0/TweenMax.min.js"></script>
+    <!-- scorll magic -->
+    <script src="//cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.5/ScrollMagic.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.5/plugins/debug.addIndicators.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.5/plugins/animation.gsap.js"></script>
+    <!--[if lt IE 9]>     
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+      <script src="js/respond.min.js"></script>
+    <![endif]-->
 
-		<!-- scorll magic -->
-		<script src="//cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.5/ScrollMagic.min.js"></script>
-		<script src="//cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.5/plugins/debug.addIndicators.min.js"></script>
-		<script src="//cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.5/plugins/animation.gsap.js"></script>
-		<!--[if lt IE 9]>			
-			<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-			<script src="js/respond.min.js"></script>
-		<![endif]-->
-	</head>
-    <body>		
-		<div class="top container">
-			<div id="trigger"></div>
-			<div class="navbar-inner main-menu span12">
-				<a href="#"><img src="images/logo.png" class="logo pull-left"></a>
-				<nav id="menu" class="pull-right">
-					<ul>
-						<li><a href="./products.html">Type</a>					
-							<ul>
-								<li><a href="./products.html">Type1</a></li>									
-								<li><a href="./products.html">Type2</a></li>
-								<li><a href="./products.html">Type3</a></li>								
-							</ul>
-						</li>															
-						<li><a href="./products.html">Place</a></li>
-						<li><a href="./products.html">Search</a>
-							<ul>
-								<li><input type="text"></li>																
-							</ul>
-						</li>
-						<li><a href="#">Login</a></li>					
-						<li><a href="register.html">Sign Up</a></li>				
-					</ul>
-				</nav>
-			</div>
-			<div id="hightlight" class="span6">
-				<h1>Food Delivery</h1>
-				<h2>We provide food that customers love, day after day after day. People just want more of it.</h2>
-			</div>
-		</div>
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+  <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+  </head>
+    <body>    
+    <div class="top container">
+      <div id="trigger"></div>
+      <div class="navbar-inner main-menu span12">
+        <a href="#"><img src="images/logo.png" class="logo pull-left"></a>
+        <nav id="menu" class="pull-right">
+          <ul>
+            <li><a href="./products.html">Type</a>          
+              <ul>
+                <li><a href="./products.html">Type1</a></li>                  
+                <li><a href="./products.html">Type2</a></li>
+                <li><a href="./products.html">Type3</a></li>                
+              </ul>
+            </li>                             
+            <li><a href="./products.html">Place</a></li>
+            <li><a href="./products.html">Search</a>
+              <ul>
+                <li><input type="text"></li>                                
+              </ul>
+            </li>
+            <li><a href="#">Login</a></li>          
+            <li><a href="register.html">Sign Up</a></li>        
+          </ul>
+        </nav>
+      </div>
+      <div id="hightlight" class="span6">
+        <h1>Food Delivery</h1>
+        <h2>We provide food that customers love, day after day after day. People just want more of it.</h2>
+      </div>
+    </div>
 
-		<div class="container">
-			<div class="content">
-				<div class="row">
-					<div id="content-header" class="span12">
-						Restaurant 8
-					</div>
-					<div class="span12 pic">
-						<div class="span4">
-							<img src="images/restaurants/res8.jpg">
-						</div>
-						<div class="span4 header-detail">
-							<div class="span8">
-								<h3>Rating : <img src="image/rating.jpg"></h3>
-							</div>
-							<div class="span8">
-								<h3>Detail : </h3>
-							</div>
-						</div>
-				</div>
-			</div>
-		</div>
-	</div>
-		</html>
+    <div class="container">
+      <div class="content">
+        <div class="row">
+          <div id="content-header" class="span12">
+            Restaurant 8
+          </div>
+          <div class="span12 pic">
+            <div class="span4">
+              <img src="images/restaurants/res8.jpg">
+            </div>
+            <div class="span4 header-detail">
+              <div class="span8">
+                <h3>Rating : <img src="image/rating.jpg"></h3>
+              </div>
+              <div class="span8">
+                <h3>Detail </h3><p>The best restaurant ever</p>
+              </div>
+            </div>
+          </div>
+          <div class="container" id="tab">
+              <ul class="nav nav-tabs">
+                <li class="active"><a data-toggle="tab" href="#Menu">Menu</a></li>
+                <li><a data-toggle="tab" href="#Comment">Comment</a></li>
+              </ul>
+
+              <div class="tab-content clearfix">
+                <div class="tab-pane fade in active" id="Menu">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                </div>
+                <div class="tab-pane fade" id="Comment">
+                    <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                </div>
+              </div>
+          </div>
+        </div>
+      </div>
+    </body>
+  </html>
