22ba9d6a678c60890bd2e6e82b33871281838bb8
THITHIRAT\Thihirat <amookiiez2309@gmail.com>
2016-11-02 22:32:50 +0700

    copybrowsebladephp

--- FDS-laravel/resources/views/browse.blade.php
+++ FDS-laravel/resources/views/browse.blade.php
@@ -18,8 +18,15 @@
 
 		<!-- scripts -->
 		<script src="js/jquery-1.7.2.min.js"></script>
-		<script src="bootstrap/js/bootstrap.min.js"></script>				
+		<script src="js/bootstrap.min.js"></script>				
 		<script src="js/superfish.js"></script>	
+
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.19.0/TweenMax.min.js"></script>
+
+		<!-- scorll magic -->
+		<script src="//cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.5/ScrollMagic.min.js"></script>
+		<script src="//cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.5/plugins/debug.addIndicators.min.js"></script>
+		<script src="//cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.5/plugins/animation.gsap.js"></script>
 		<!--[if lt IE 9]>			
 			<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
 			<script src="js/respond.min.js"></script>
@@ -27,27 +34,30 @@
 	</head>
     <body>		
 		<div class="top container">
-			<div class="navbar-inner main-menu span12">
-				<a href="#"><img src="images/logo.png" class="logo pull-left"></a>
-				<nav id="menu" class="pull-right">
-					<ul>
-						<li><a href="./products.html">Type</a>					
-							<ul>
-								<li><a href="./products.html">Type1</a></li>									
-								<li><a href="./products.html">Type2</a></li>
-								<li><a href="./products.html">Type3</a></li>								
-							</ul>
-						</li>															
-						<li><a href="./products.html">Place</a></li>
-						<li><a href="./products.html">Search</a>
-							<ul>
-								<li><input type="text"></li>																
-							</ul>
-						</li>
-						<li><a href="#">Login</a></li>					
-						<li><a href="register.html">Sign Up</a></li>				
-					</ul>
-				</nav>
+			<div id="trigger"></div>
+			<div class="navbar navbar-fixed-top">		
+				<div class="navbar-inner main-menu span12">
+					<a href="#"><img src="images/logo.png" class="logo pull-left"></a>
+					<nav id="menu" class="pull-right">
+						<ul>
+							<li><a href="./products.html">Type</a>					
+								<ul>
+									<li><a href="./products.html">Type1</a></li>									
+									<li><a href="./products.html">Type2</a></li>
+									<li><a href="./products.html">Type3</a></li>								
+								</ul>
+							</li>															
+							<li><a href="./products.html">Place</a></li>
+							<li><a href="./products.html">Search</a>
+								<ul>
+									<li><input type="text"></li>																
+								</ul>
+							</li>
+							<li><a href="#">Login</a></li>					
+							<li><a href="register.html">Sign Up</a></li>				
+						</ul>
+					</nav>
+				</div>
 			</div>
 			<div id="hightlight" class="span6">
 				<h1>Food Delivery</h1>
@@ -64,76 +74,112 @@
 				<div class="row">
 					<div id="menuList" class="span12">
 						<div class="span4 item">
-							<img src="images/restaurants/res2.jpg">
+							<div class="img-contain">
+								<img src="images/restaurants/res2.jpg">
+							</div>
 							<div class="span4 item-text">
 								<h1>Restaurant 1</h1>
 							</div>
+							<div class="triangle"></div>
 						</div>
 						<div class="span4 item">
-							<img src="images/restaurants/res3.jpg">
+							<div class="img-contain">
+								<img src="images/restaurants/res3.jpg">
+							</div>
 							<div class="span4 item-text">
 								<h1>Restaurant 2</h1>
 							</div>
+							<div class="triangle"></div>
 						</div>
 						<div class="span4 item">
-							<img src="images/restaurants/res6.jpg">
+							<div class="img-contain">
+								<img src="images/restaurants/res4.jpg">
+							</div>
 							<div class="span4 item-text">
 								<h1>Restaurant 3</h1>
 							</div>
+							<div class="triangle"></div>
 						</div>
 						<div class="span4 item">
-							<img src="images/restaurants/res5.jpg">
+							<div class="img-contain">
+								<img src="images/restaurants/res5.jpg">
+							</div>
 							<div class="span4 item-text">
 								<h1>Restaurant 4</h1>
 							</div>
+							<div class="triangle"></div>
 						</div>
 						<div class="span4 item">
-							<img src="images/restaurants/res2.jpg">
+							<div class="img-contain">
+								<img src="images/restaurants/res6.jpg">
+							</div>
 							<div class="span4 item-text">
 								<h1>Restaurant 1</h1>
 							</div>
+							<div class="triangle"></div>
 						</div>
 						<div class="span4 item">
-							<img src="images/restaurants/res3.jpg">
+							<div class="img-contain">
+								<img src="images/restaurants/res7.jpg">
+							</div>
 							<div class="span4 item-text">
 								<h1>Restaurant 2</h1>
 							</div>
+							<div class="triangle"></div>
 						</div>
 						<div class="span4 item">
-							<img src="images/restaurants/res6.jpg">
+							<div class="img-contain">
+								<img src="images/restaurants/res2.jpg">
+							</div>
 							<div class="span4 item-text">
 								<h1>Restaurant 3</h1>
 							</div>
+							<div class="triangle"></div>
 						</div>
 						<div class="span4 item">
-							<img src="images/restaurants/res5.jpg">
+							<div class="img-contain">
+								<img src="images/restaurants/res4.jpg">
+							</div>
 							<div class="span4 item-text">
 								<h1>Restaurant 4</h1>
 							</div>
+							<div class="triangle"></div>
 						</div>
 						<div class="span4 item">
-							<img src="images/restaurants/res2.jpg">
+							<div class="img-contain">
+								<img src="images/restaurants/res3.jpg">
+							</div>
 							<div class="span4 item-text">
 								<h1>Restaurant 1</h1>
 							</div>
+							<div class="triangle"></div>
 						</div>
 						<div class="span4 item">
-							<img src="images/restaurants/res3.jpg">
+							<div class="img-contain">
+								<img src="images/restaurants/res4.jpg">
+							</div>
 							<div class="span4 item-text">
 								<h1>Restaurant 2</h1>
 							</div>
+							<div class="triangle"></div>
 						</div>
 						<div class="span4 item">
-							<img src="images/restaurants/res6.jpg">
+							<div class="img-contain">
+								<img src="images/restaurants/res5.jpg">
+							</div>
 							<div class="span4 item-text">
 								<h1>Restaurant 3</h1>
 							</div>
+							<div class="triangle"></div>
 						</div>
 						<div class="span4 item">
-							<img src="images/restaurants/res5.jpg">
+							<div class="img-contain">
+								<img src="images/restaurants/res6.jpg">
+							</div>
 							<div class="span4 item-text">
 								<h1>Restaurant 4</h1>
 							</div>
+							<div class="triangle"></div>
 						</div>
 					</div>
 				</div>
@@ -143,5 +189,7 @@
 
 		</div>
 		<script src="js/common.js"></script>
+		<script src="js/browse.js"></script>
+		<script src="js/scroll.js"></script>
     </body>
 </html>
