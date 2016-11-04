ff28d1ff224b60e9ffa6fb3b46e8ef83835eba68
THITHIRAT\Thihirat <amookiiez2309@gmail.com>
2016-11-04 18:07:58 +0700

    setframe

--- FDS-laravel/resources/views/checkout.blade.php
+++ FDS-laravel/resources/views/checkout.blade.php
@@ -7,7 +7,7 @@
         <meta name="description" content="">
         <!--[if ie]><meta content='IE=8' http-equiv='X-UA-Compatible'/><![endif]-->
         <!-- bootstrap -->
-              
+        <!-- <link href="css/bootstrap.min.css" rel="stylesheet">  -->     
         <link href="css/bootstrap-responsive.min.css" rel="stylesheet">
         
         <link href="css/bootstrappage.css" rel="stylesheet"/>
@@ -18,106 +18,123 @@
 
         <!-- scripts -->
         <script src="js/jquery-1.7.2.min.js"></script>
-        <script src="js/bootstrap.min.js"></script>               
+        <script src="js/bootstrap.min.js"></script>             
         <script src="js/superfish.js"></script> 
+
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.19.0/TweenMax.min.js"></script>
+
+        <!-- scorll magic -->
+        <script src="//cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.5/ScrollMagic.min.js"></script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.5/plugins/debug.addIndicators.min.js"></script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.5/plugins/animation.gsap.js"></script>
         <!--[if lt IE 9]>           
             <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
             <script src="js/respond.min.js"></script>
         <![endif]-->
-         <link href="css/checkout.css" rel="stylesheet">
-         <script src="js/checkout.js"></script> 
-
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     </head>
     <body>      
         <div class="top container">
-            <div class="navbar-inner main-menu span12">
-                <a href="#"><img src="images/logo.png" class="logo pull-left"></a>
-                <nav id="menu" class="pull-right">
-                    <ul>
-                        <li><a href="./products.html">Type</a>                  
-                            <ul>
-                                <li><a href="./products.html">Type1</a></li>                                    
-                                <li><a href="./products.html">Type2</a></li>
-                                <li><a href="./products.html">Type3</a></li>                                
-                            </ul>
-                        </li>                                                           
-                        <li><a href="./products.html">Place</a></li>
-                        <li><a href="./products.html">Search</a>
-                            <ul>
-                                <li><input type="text"></li>                                                                
-                            </ul>
-                        </li>
-                        <li><a href="#">Login</a></li>                  
-                        <li><a href="register.html">Sign Up</a></li>                
-                    </ul>
-                </nav>
+            <div id="trigger"></div>
+            <div class="navbar navbar-fixed-top">       
+                <div class="navbar-inner main-menu span12">
+                    <a href="#"><img src="images/logo.png" class="logo pull-left"></a>
+                    <nav id="menu" class="pull-right">
+                        <ul>
+                            <li><a href="./products.html">Type</a>                  
+                                <ul>
+                                    <li><a href="./products.html">Type1</a></li>                                    
+                                    <li><a href="./products.html">Type2</a></li>
+                                    <li><a href="./products.html">Type3</a></li>                                
+                                </ul>
+                            </li>                                                           
+                            <li><a href="./products.html">Place</a></li>
+                            <li><a href="./products.html">Search</a>
+                                <ul>
+                                    <li><input type="text"></li>                                                                
+                                </ul>
+                            </li>
+                            <li><a href="#">Login</a></li>                  
+                            <li><a href="register.html">Sign Up</a></li>                
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+            <div id="hightlight" class="span6">
+                <h1>Food Delivery</h1>
+                <h2>We provide food that customers love, day after day after day. People just want more of it.</h2>
             </div>
         </div>
 
-        <div class="mar-left20"> 
-            <h1>Restaurant Name</h1>
+        <div class="container">
+            <div class="content">
+                <div class="row">
+                    <div id="content-header" class="span12">
+                        Restaurant Name
+                    </div>
+                </div>
+                <div>
+                    <table id="t01" style="width:70%" class="span12">        
+                        <tr>
+                            <th>Menu</th>
+                            <th>Quantity</th>
+                            <th>Price</th>
+                            <th></th>    
+                        </tr>
+                        <tr class="text-center">
+                            <td class="text-left">Jill</td>
+                            <td>
+                                <div>
+                                    <div class="qty-changer">
+                                        <input class="qty-input form-group" type="number" value="1"/>
+                                    </div>
+                                </div>
+                            </td>
+                            <td>50</td>
+                            <td>
+                                <span class="glyphicon glyphicon-remove btn-danger"></span>
+                            </td>
+                        </tr>
+                        <tr class="text-center">
+                            <td class="text-left">Eve</td>
+                            <td>
+                                <div>
+                                    <div class="qty-changer">
+                                        <input class="qty-input form-group" type="number" value="1"/>
+                                    </div>
+                                </div>
+                            </td>
+                            <td>44</td>
+                            <td>
+                                <span class="glyphicon glyphicon-remove btn-danger"></span>
+                            </td>
+                        </tr>
+                    </table>
+                    <br><br><br><br><br><br><br>
+                    <form>
+                        <font size="4">
+                            <strong> Delivery Methods </strong>
+                        </font>
+                        <font size="3">
+                            <br><br>
+                            <input type="radio" name="gender" value="male" checked> Delivery
+                            <br><br>
+                            <input type="radio" name="gender" value="female"> Fast Delivery (+50 baht)
+                            <br><br>
+                        </font>
+                    </form>
+                    <div align="right">
+                        <button type="button" class="btn btn-default">Confirm</button>
+                    </div>    
+                </div>       
+            </div>
         </div>
 
-        <div class="mar-left20">
-            <table id="t01" style="width:70%" class="span12">        
-              <tr>
-                <th>Menu</th>
-                <th>Quantity</th>
-                <th>Price</th>
-                <th></th>    
-              </tr>
-              <tr class="text-center">
-                <td class="text-left">Jill</td>
-                <td>
-                    <div>
-                        <div class="qty-changer">
-                        <input class="qty-input form-group" type="number" value="1"/>
-                        </div>
-                    </div>
-                </td>
-                <td>50</td>
-                <td>
-                    <span class="glyphicon glyphicon-remove btn-danger"></span>
-                </td>
-              </tr>
-              <tr class="text-center">
-                <td class="text-left">Eve</td>
-                <td>
-                    <div>
-                        <div class="qty-changer">
-                        <input class="qty-input form-group" type="number" value="1"/>
-                        </div>
-                    </div>
-                </td>
-                <td>44</td>
-                <td>
-                    <span class="glyphicon glyphicon-remove btn-danger"></span>
-                </td>
-              </tr>
-            </table>
+        <div class="footer container">
+
         </div>
-        <br><br><br><br><br><br><br>
-        <div class="mar-left20">
-            <form>
-                <font size="5.5">
-                <strong> Delivery Methods </strong>
-                </font>
-                <font size="5">
-                <br><br>
-                <input type="radio" name="gender" value="male" checked> Delivery
-                <br><br>
-                <input type="radio" name="gender" value="female"> Fast Delivery (+50 baht)
-                <br>
-                </font>
-            </form> 
-        </div>
-        <br>
-        <div align="center">
-            <button type="button" class="btn btn-default">Confirm</button>
-        </div>    
         <script src="js/common.js"></script>
+        <script src="js/browse.js"></script>
+        <script src="js/scroll.js"></script>
     </body>
 </html>
