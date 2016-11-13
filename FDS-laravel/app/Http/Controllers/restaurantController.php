1cf55c1a9b30668a96a983d01669ef55abbf2f59
nonamexx <mrkom9@gmail.com>
2016-11-13 16:16:57 +0700

    Database
    Image from DB
    Change style of page

--- FDS-laravel/app/Http/Controllers/restaurantController.php
+++ FDS-laravel/app/Http/Controllers/restaurantController.php
@@ -9,17 +9,15 @@ class restaurantController extends Controller
 {
     //
     public function index(){
-    	$restaurant = DB::table('restaurant')->where('restaurant_id',1)->get();
+    	$restaurant = DB::table('restaurant')   -> where('restaurant_id',1)
+                                                -> get();
+
     	return view('restaurant',['restaurants' => $restaurant]);
     }
 
     public function restaurant($restaurant_id){
-    	$restaurant = DB::table('restaurant')->where('restaurant_id',$restaurant_id)->get();
+    	$restaurant = DB::table('restaurant')   -> where('restaurant_id',$restaurant_id)
+                                                -> get();
 	    return view('restaurant',['restaurants' => $restaurant]);
     }
 }
-/*
-Route::get('/restaurant/{restaurant_id}', function ($restaurant_id) {
-    return view('restaurant'.$restaurant_id);
-});
-*/
--- FDS-laravel/resources/views/restaurant.blade.php
+++ FDS-laravel/resources/views/restaurant.blade.php
@@ -1,36 +1,42 @@
 <html lang="en">
   <head>
+    <link href="css/bootstrap.min.css" rel="stylesheet">      
+    <link href="css/restaurant.css" rel="stylesheet"/>       
+
     <meta charset="utf-8">
     <title>Software Engineer</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <!--[if ie]><meta content='IE=8' http-equiv='X-UA-Compatible'/><![endif]-->
     <!-- bootstrap -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">      
-    <link href="css/bootstrap-responsive.min.css" rel="stylesheet">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
     
-    <link href="css/bootstrappage.css" rel="stylesheet"/>
+
+    <link href="{{ asset('css/bootstrap-responsive.min.css') }}" rel="stylesheet">
+    
+    <link href="{{ asset('css/bootstrappage.css') }}" rel="stylesheet"/>
     
     <!-- global styles -->
-    <link href="css/flexslider.css" rel="stylesheet"/>
-    <link href="css/browse.css" rel="stylesheet"/>
+    <link href="{{ asset('css/flexslider.css') }}" rel="stylesheet"/>
+    <link href="{{ asset('css/nav.css') }}" rel="stylesheet"/>
+    <link href="{{ asset('css/browse.css') }}" rel="stylesheet"/>
 
-    <link href="css/restaurant.css" rel="stylesheet"/>
     <!-- scripts -->
-    <script src="js/jquery-1.7.2.min.js"></script>
-    <script src="bootstrap/js/bootstrap.min.js"></script>       
-    <script src="js/superfish.js"></script> 
+    <script src="{{ asset('js/jquery-1.7.2.min.js') }}"></script>     
+    <script src="{{ asset('js/superfish.js') }}"></script>  
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script> 
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.19.0/TweenMax.min.js"></script>
-
+    <script src="js/bootstrap.min.js"></script>  
     <!-- scorll magic -->
     <script src="//cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.5/ScrollMagic.min.js"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.5/plugins/debug.addIndicators.min.js"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.5/plugins/animation.gsap.js"></script>
-    <!--[if lt IE 9]>     
-      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-      <script src="js/respond.min.js"></script>
-    <![endif]-->
 
       <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
   <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
@@ -39,27 +45,55 @@
     <body>    
     <div class="top container">
       <div id="trigger"></div>
-      <div class="navbar-inner main-menu span12">
-        <a href="#"><img src="images/logo.png" class="logo pull-left"></a>
-        <nav id="menu" class="pull-right">
-          <ul>
-            <li><a href="./products.html">Type</a>          
-              <ul>
-                <li><a href="./products.html">Type1</a></li>                  
-                <li><a href="./products.html">Type2</a></li>
-                <li><a href="./products.html">Type3</a></li>                
-              </ul>
-            </li>                             
-            <li><a href="./products.html">Place</a></li>
-            <li><a href="./products.html">Search</a>
-              <ul>
-                <li><input type="text"></li>                                
-              </ul>
-            </li>
-            <li><a href="#">Login</a></li>          
-            <li><a href="register.html">Sign Up</a></li>        
-          </ul>
-        </nav>
+      <div class="navbar navbar-fixed-top">   
+        <div class="navbar-inner main-menu span12">
+          <a href="#"><img src="{{ asset('images/logo.png') }}" class="logo pull-left"></a>
+          <nav id="menu" class="pull-right">
+            <ul>
+              <li><a href="{{ url('browse') }}">Browse</a></li>
+              <li><a href="#">Meal</a>          
+                <ul>
+                  <li><a href="{{ url('/browse/vegan') }}">Vegan</a></li>                 
+                  <li><a href="{{ url('/browse/islamic') }}">Isalmic</a></li>
+                  <li><a href="{{ url('/browse/meal') }}">All Meal</a></li>               
+                </ul>
+              </li>                             
+              <li><a href="{{ url('/browse/dessert') }}">Dessert</a></li>
+              <li><a href="{{ url('/browse/drink') }}">Drink</a></li>
+              <li><a href="#">Search</a>
+                <ul>
+                  <li id="search">
+                  <form method="get" action="{{ url('/browse/search/') }}">
+                    <input type="search" name="search" placeholder="search">
+                    <input type="submit" style="display:none;"/>
+                  </form>
+                  </li>
+
+                </ul>
+              </li>
+              <li>
+              @if (Auth::check())
+                <a href="#">{{Auth::user()->username}}</a>
+                <ul>
+                  <li>
+                    <a href="{{ url('logout') }}"
+                                        onclick="event.preventDefault();
+                                                 document.getElementById('logout-form').submit();">
+                                        Logout
+                                    </a>
+
+                                    <form id="logout-form" action="{{ url('logout') }}" method="POST" style="display: none;">
+                                        {{ csrf_field() }}
+                                    </form>
+                  </li>
+                </ul>
+               </li>  
+              @else
+                <a href="{{ url('login') }}">Login</a></li>
+              @endif          
+            </ul>
+          </nav>
+        </div>
       </div>
       <div id="hightlight" class="span6">
         <h1>Food Delivery</h1>
@@ -79,11 +113,14 @@
           </div>
           <div class="span12 pic">
             <div class="span4">
-              <img src="images/restaurants/res8.jpg">
+              <img src="{{ asset('images/restaurants/res'.$restaurant->restaurant_id.'.jpg') }}">
             </div>
             <div class="span4 header-detail">
               <div class="span8">
-                <h3>Address </h3><p>{{$restaurant->restaurant_address}}</p>
+                <h3>Address </h3>
+                <p>{{$restaurant->restaurant_address}}</p>
+                <h3>Phone</h3>
+                <p>{{$restaurant->restaurant_phone}}</p>
               </div>
             </div>
           </div>
@@ -235,5 +272,9 @@
           </div>
         </div>
       </div>
+
+      <script type="text/javascript">
+
+      </script>
     </body>
   </html>
