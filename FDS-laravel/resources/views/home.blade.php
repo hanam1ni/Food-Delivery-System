3090db50c9186b9c8d1246854435d089a2da2d37
cooky <k.h.j.g.l.d@gmail.com>
2016-11-14 22:04:26 +0700

    commit real page

--- FDS-laravel/resources/views/home.blade.php
+++ FDS-laravel/resources/views/home.blade.php
@@ -24,8 +24,28 @@
             <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
             <script src="js/respond.min.js"></script>
         <![endif]-->
-         <link href="css/home.css" rel="stylesheet">
-         <script src="js/checkout.js"></script> 
+         <link href="css/home.css" rel="stylesheet"/>
+         <script language="javascript" type="text/javascript" src="{{asset('js/mootools-1.2.1-core.js')}}"></script>
+		<script language="javascript" type="text/javascript" src="{{asset('js/mootools-1.2-more.js')}}"></script>
+		<script language="javascript" type="text/javascript" src="{{asset('js/slideitmoo-1.1.js')}}"></script>
+		<script language="javascript" type="text/javascript">
+		window.addEvents({
+			'domready': function () { /* thumbnails example , div containers */
+				new SlideItMoo({
+					overallContainer: 'SlideItMoo_outer',
+					elementScrolled: 'SlideItMoo_inner',
+					thumbsContainer: 'SlideItMoo_items',
+					itemsVisible: 5,
+					elemsSlide: 3,
+					duration: 200,
+					itemsSelector: '.SlideItMoo_element',
+					itemWidth: 140,
+					showControls: 1
+				});
+			},
+
+		});
+		</script>
     </head>
     <body>      
         <div class="top container">
@@ -54,22 +74,23 @@
         <div id="hightlight" class="span6">
 				<h1>Food Delivery</h1>
 				<h2>We provide food that customers love, day after day after day. People just want more of it.</h2>
-			</div>
+		</div>
+		</div>
 		<div id="newgallary">
       <h2>What's NEW</h2>
       <div id="SlideItMoo_outer" style="width: 750px;">
         <div id="SlideItMoo_inner" style="width: 700px;">
           <div id="SlideItMoo_items" style="width: 1200px; margin-left: 0px;">
-            <div class="SlideItMoo_element"> <a href="#"> <img src="images/res1.jpg" alt=""></a> </div>
-			<div class="SlideItMoo_element"> <a href="#"> <img src="images/res2.jpg" alt=""></a> </div>
-			<div class="SlideItMoo_element"> <a href="#"> <img src="images/res3.jpg" alt=""></a> </div>
-			<div class="SlideItMoo_element"> <a href="#"> <img src="images/res4.jpg" alt=""></a> </div>
-			<div class="SlideItMoo_element"> <a href="#"> <img src="images/res5.jpg" alt=""></a> </div>
-			<div class="SlideItMoo_element"> <a href="#"> <img src="images/res6.jpg" alt=""></a> </div>
-			<div class="SlideItMoo_element"> <a href="#"> <img src="images/res7.jpg" alt=""></a> </div>
-                      
-          </div>
+            <div class="SlideItMoo_element"> <a href="#"> <img src="images/restaurants/res3.jpg" alt="" /></a> </div>
+            <div class="SlideItMoo_element"> <a href="#"> <img src="images/restaurants/res7.jpg" alt="" /></a> </div>
+            <div class="SlideItMoo_element"> <a href="#"> <img src="images/restaurants/res6.jpg" alt="" /></a> </div>
+            <div class="SlideItMoo_element"> <a href="#"> <img src="images/restaurants/res5.jpg" alt="" /></a> </div>
+			<div class="SlideItMoo_element"> <a href="#"> <img src="images/restaurants/res8.jpg" alt="" /></a> </div>
+			<div class="SlideItMoo_element"> <a href="#"> <img src="images/restaurants/res9.jpg" alt="" /></a> </div>
+			<div class="SlideItMoo_element"> <a href="#"> <img src="images/restaurants/res10.jpg" alt="" /></a> </div>
+            </div>
         </div>
-      <div class="SlideItMoo_forward"></div><div class="SlideItMoo_back"></div></div>
+      </div>
+    
     </div>
-	   </div>
+	</body>
