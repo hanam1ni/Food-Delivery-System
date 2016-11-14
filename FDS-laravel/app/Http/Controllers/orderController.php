8dfd609f46e24a8e000649934b5ac5a87afd8ae3
hanam1ni <immicky.a@gmail.com>
2016-11-15 01:28:45 +0700

    Last F

--- FDS-laravel/app/Http/Controllers/orderController.php
+++ FDS-laravel/app/Http/Controllers/orderController.php
@@ -16,16 +16,6 @@ class orderController extends Controller
 
     public function sendOrder(Request $request)
     {
-    	$values = $request->session()->get('basket.list', 'default');
-        foreach ($values as $value) {
-            $newOrder                = new OrderFood;
-            $newOrder->order_id		 = Auth::user()->id;
-            $newOrder->food_id       = $value['food_id'];
-            $newOrder->restaurant_id = $value['restaurant_id'];
-            $newOrder->user_id       = Auth::user()->id;
-            $newOrder->order_status  = 0;
-            $newOrder->save();
-        }
         $request->session()->forget('basket.list');
         return redirect('/order/view/');
     }
